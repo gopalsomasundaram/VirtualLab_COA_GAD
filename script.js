c897dcc6c8773c73af1979fd611b8172a7f8c8a2
gopalsomasundaram <62381540+gopalsomasundaram@users.noreply.github.com>
2023-10-04 09:26:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
